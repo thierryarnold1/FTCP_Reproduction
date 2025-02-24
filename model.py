d29b57f01b96c9ba4bacb251d2c7693c5065d19d
thierryarnold1 <140014431+thierryarnold1@users.noreply.github.com>
2025-02-24 18:56:34 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,49 @@
 import tensorflow as tf
-from keras import backend as K
-from keras.layers import Input, Dense, Lambda, Conv1D, Conv2DTranspose, \
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input, Dense, Lambda, Conv1D, Conv2DTranspose, \
     LeakyReLU, Activation, Flatten, Reshape, BatchNormalization
-from keras import layers
-from keras.models import Model
+from tensorflow.keras.models import Model
+
+# --- Custom KL Divergence Layer ---
+class KLDivergenceLayer(tf.keras.layers.Layer):
+    def __init__(self, coeff, **kwargs):
+        super(KLDivergenceLayer, self).__init__(**kwargs)
+        self.coeff = coeff
+
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        kl_loss = -0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        self.add_loss(self.coeff * kl_loss)
+        # Pass through one of the inputs unchanged.
+        return z_mean
+
+# --- Custom Property Loss Layer ---
+class PropertyLossLayer(tf.keras.layers.Layer):
+    def __init__(self, coeff, prop_dim, **kwargs):
+        super(PropertyLossLayer, self).__init__(**kwargs)
+        self.coeff = coeff
+        self.prop_dim = prop_dim
+
+    def call(self, inputs):
+        reg_in, y_hat_val = inputs
+        # Compute property loss on the first 'prop_dim' elements.
+        loss = K.sum(K.square(reg_in[:, :self.prop_dim] - y_hat_val))
+        self.add_loss(self.coeff * loss)
+        return y_hat_val
+
+# --- Custom Semi-supervised Property Loss Layer ---
+class SemiPropertyLossLayer(tf.keras.layers.Layer):
+    def __init__(self, coeff, prop_dim, semi_prop_dim, **kwargs):
+        super(SemiPropertyLossLayer, self).__init__(**kwargs)
+        self.coeff = coeff
+        self.prop_dim = prop_dim
+        self.semi_prop_dim = semi_prop_dim
+
+    def call(self, inputs):
+        y_semi, y_semi_hat_val = inputs
+        loss = K.sum(K.square(y_semi_hat_val - y_semi[:, self.prop_dim:self.prop_dim+self.semi_prop_dim]))
+        self.add_loss(self.coeff * loss)
+        return y_semi_hat_val
 
 def FTCP(X_train, y_train, coeffs=(2, 10,), semi=False, label_ind=None, prop_dim=None):
     
@@ -16,8 +56,8 @@ def FTCP(X_train, y_train, coeffs=(2, 10,), semi=False, label_ind=None, prop_dim
     
     latent_dim = 256
     max_filters = 128
-    filter_size = [5,3,3]
-    strides = [2,2,1]
+    filter_size = [5, 3, 3]
+    strides = [2, 2, 1]
     
     input_dim = X_train.shape[1]
     channel_dim = X_train.shape[2]
@@ -33,7 +73,7 @@ def FTCP(X_train, y_train, coeffs=(2, 10,), semi=False, label_ind=None, prop_dim
         
         label_ind = tf.convert_to_tensor(label_ind, dtype=tf.int64)
         def get_idn(y):
-            y_ind = y[:,-1]  
+            y_ind = y[:, -1]  
             y_ind = tf.dtypes.cast(y_ind, tf.int64)
             com_ind = tf.sets.intersection(y_ind[None, :], label_ind[None, :])
             com_ind = tf.sparse.to_dense(com_ind)
@@ -43,67 +83,78 @@ def FTCP(X_train, y_train, coeffs=(2, 10,), semi=False, label_ind=None, prop_dim
             return semi_ind
         semi_ind = Lambda(get_idn)(regression_inputs)
         
-    x = Conv1D(max_filters//4, filter_size[0], strides=strides[0], padding='SAME')(encoder_inputs)
+    # Encoder
+    x = Conv1D(max_filters//4, filter_size[0], strides=strides[0], padding='same')(encoder_inputs)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
-    x = Conv1D(max_filters//2, filter_size[1], strides=strides[1], padding='SAME')(x)
+    x = Conv1D(max_filters//2, filter_size[1], strides=strides[1], padding='same')(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
-    x = Conv1D(max_filters, filter_size[2], strides=strides[2], padding='SAME')(x)
+    x = Conv1D(max_filters, filter_size[2], strides=strides[2], padding='same')(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
     x = Flatten()(x)
     x = Dense(1024, activation='sigmoid')(x)
-    z_mean = Dense(latent_dim,activation = 'linear')(x)
-    z_log_var = Dense(latent_dim,activation = 'linear')(x)
+    z_mean = Dense(latent_dim, activation='linear')(x)
+    z_log_var = Dense(latent_dim, activation='linear')(x)
     
+    # Reparameterization
     def sampling(args):
         z_mean, z_log_var = args
         epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0, stddev=1)
-        return z_mean+K.exp(z_log_var/2)*epsilon
+        return z_mean + K.exp(z_log_var/2) * epsilon
     
-    # Reparameterization
     z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+    
+    # Wrap KL divergence in a custom layer.
+    _ = KLDivergenceLayer(coeff_KL)([z_mean, z_log_var])
+    
     encoder = Model(encoder_inputs, z, name='encoder')
     
+    # Regression branch (for property prediction)
     if not semi:
-        x = Activation('relu')(z_mean)
-        x = Dense(128, activation="relu")(x)
-        x = Dense(32, activation="relu")(x)
-        y_hat = Dense(regression_dim, activation ='sigmoid')(x)
+        x_reg = Activation('relu')(z_mean)
+        x_reg = Dense(128, activation="relu")(x_reg)
+        x_reg = Dense(32, activation="relu")(x_reg)
+        y_hat = Dense(regression_dim, activation='sigmoid')(x_reg)
+        # Wrap property loss computation in a custom layer.
+        y_hat = PropertyLossLayer(coeff_prop, prop_dim)([regression_inputs, y_hat])
         regression = Model(encoder_inputs, y_hat, name='target-learning branch')
     else:
-        x = Activation('relu')(z_mean)
-        x = Dense(128, activation="relu")(x)
-        x = Dense(32, activation="relu")(x)
-        y_hat = Dense(prop_dim, activation ='sigmoid')(x)
+        x_reg = Activation('relu')(z_mean)
+        x_reg = Dense(128, activation="relu")(x_reg)
+        x_reg = Dense(32, activation="relu")(x_reg)
+        y_hat = Dense(prop_dim, activation='sigmoid')(x_reg)
+        # Add property loss for the supervised part.
+        y_hat = PropertyLossLayer(coeff_prop, prop_dim)([regression_inputs, y_hat])
         
-        x = Activation('relu')(z_mean)
-        x = Dense(128, activation="relu")(x)
-        x = Dense(32, activation="relu")(x)
-        y_semi_hat = Dense(semi_prop_dim, activation ='sigmoid')(x)
+        x_semi = Activation('relu')(z_mean)
+        x_semi = Dense(128, activation="relu")(x_semi)
+        x_semi = Dense(32, activation="relu")(x_semi)
+        y_semi_hat = Dense(semi_prop_dim, activation='sigmoid')(x_semi)
         regression = Model(encoder_inputs, [y_hat, y_semi_hat], name='target-learning branch')
         
+        # Gather semi-supervised indices.
         y_semi = Lambda(lambda x: tf.gather(x, semi_ind, axis=0))(regression_inputs)
         y_semi_hat = Lambda(lambda x: tf.gather(x, semi_ind, axis=0))(y_semi_hat)
+        # Wrap semi-supervised property loss in a custom layer.
+        _ = SemiPropertyLossLayer(coeff_prop_semi, prop_dim, semi_prop_dim)([y_semi, y_semi_hat])
     
+    # Decoder
     latent_inputs = Input(shape=(latent_dim,))
     map_size = K.int_shape(encoder.layers[-6].output)[1]
-    x = Dense(max_filters*map_size, activation='relu')(latent_inputs)
-    x = Reshape((map_size, 1, max_filters))(x)
-    x = BatchNormalization()(x)
-    x = Conv2DTranspose(max_filters//2, (filter_size[2], 1), strides=(strides[2], 1), 
-                        padding='SAME')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2DTranspose(max_filters//4, (filter_size[1], 1), strides=(strides[1], 1), 
-                        padding='SAME')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv2DTranspose(channel_dim, (filter_size[0],1), strides=(strides[0],1), 
-                        padding='SAME')(x)
-    x = Activation('sigmoid')(x)
-    decoder_outputs = Lambda(lambda x: K.squeeze(x, axis=2))(x)
+    x_dec = Dense(max_filters * map_size, activation='relu')(latent_inputs)
+    x_dec = Reshape((map_size, 1, max_filters))(x_dec)
+    x_dec = BatchNormalization()(x_dec)
+    x_dec = Conv2DTranspose(max_filters//2, (filter_size[2], 1), strides=(strides[2], 1), padding='same')(x_dec)
+    x_dec = BatchNormalization()(x_dec)
+    x_dec = Activation('relu')(x_dec)
+    x_dec = Conv2DTranspose(max_filters//4, (filter_size[1], 1), strides=(strides[1], 1), padding='same')(x_dec)
+    x_dec = BatchNormalization()(x_dec)
+    x_dec = Activation('relu')(x_dec)
+    x_dec = Conv2DTranspose(channel_dim, (filter_size[0], 1), strides=(strides[0], 1), padding='same')(x_dec)
+    x_dec = Activation('tanh')(x_dec)
+    decoder_outputs = Lambda(lambda x: K.squeeze(x, axis=2))(x_dec)
     decoder = Model(latent_inputs, decoder_outputs, name='decoder')
     
     reconstructed_outputs = decoder(z)
@@ -111,16 +162,9 @@ def FTCP(X_train, y_train, coeffs=(2, 10,), semi=False, label_ind=None, prop_dim
     
     VAE.summary()
     
-    def vae_loss(x, decoded_x):
-        loss_recon = K.sum(K.square(encoder_inputs - reconstructed_outputs))
-        loss_KL = -0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-        loss_prop = K.sum(K.square(regression_inputs[:, :prop_dim] - y_hat))
-        
-        if semi:
-            loss_prop_semi = K.sum(K.square(y_semi_hat - y_semi[:, prop_dim:prop_dim+semi_prop_dim]))
-            vae_loss = K.mean(loss_recon + coeff_KL*loss_KL + coeff_prop*loss_prop + coeff_prop_semi*loss_prop_semi)
-        else:
-            vae_loss = K.mean(loss_recon + coeff_KL*loss_KL + coeff_prop*loss_prop)
-        return vae_loss
+    # Custom loss now only computes the reconstruction loss.
+    def vae_loss(y_true, y_pred):
+        loss_recon = K.sum(K.square(y_true - y_pred))
+        return K.mean(loss_recon)
     
     return VAE, encoder, decoder, regression, vae_loss
