15993ba7aa68a2c86771502da4e75132806279cf
thierryarnold1 <140014431+thierryarnold1@users.noreply.github.com>
2025-03-04 13:16:04 -0500

    Update data.py

--- data.py
+++ data.py
@@ -62,7 +62,7 @@ def data_query(mp_api_key):
             "stability_discharge": result.stability_discharge,
             "id_charge": result.id_charge,
             "id_discharge": result.id_discharge,
-            "cif": result.id_discharge.structure.to(fmt="cif") if result.id_discharge else None
+            "cif": result.host_structure.to(fmt="cif") if result.host_structure else None
         }
         data.append(entry)
 
